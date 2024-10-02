0bfebdc9f7f89637e37d665e5f2ff6e55105fdce
Juan Galvis <juan_galvis82232@elpoli.edu.co>
2024-10-02 14:12:09 -0500

    intentado comitiar de nuevo

--- laboratorio/src/tablas.java
+++ laboratorio/src/tablas.java
@@ -2,7 +2,15 @@
 import java.util.Scanner;
 
 public class tablas{
-        
+    static int b;
+    static  double[] distancia;
+    static double[] suma, promedio;
+    static double[] velocidad; 
+    static double[] aceleracion;
+    static double []desviacion;
+    static double[][] tiempos;
+    static double[] acceGravedad;
+    static double[] porcen;
         public static void main(String args[]) {
         
             menu();
@@ -40,69 +48,16 @@ public class tablas{
         //Metodo de la tabla del MRUA
         private static void MRUA() {
             
-            //con los intervalos me refiero a en cuanto dividieron la distancia
-            
-            @SuppressWarnings("resource")
-            Scanner Scanner= new Scanner(System.in);
-            
-            int b;
-            System.out.print("cuantos intervalos de tiempo?: ");
-            b=Scanner.nextInt();
-            
-            double[] distancia=new double[5];
-            double[] suma=new double[b], promedio=new double[b];
-            double[] velocidad=new double[b]; 
-            double[] aceleracion=new double[b];
-            double []desviacion=new double[b];
-            double[][] tiempos=new double[b][5];
-            
-            
-            //fill out the distance and the time
-            
-            System.out.println("digite la distancia y luego los tiempos de seguido por intervalo");
-            for(int i=0;i<b;i++){
-                System.out.println("intervalo: "+(i+1));
-                System.out.print("\ndistancia: ");
-                distancia[i]=Scanner.nextDouble();
-                
-                System.out.print("\ntiempos: ");
-                for(int j=0;j<5;j++){
-                        tiempos[i][j]=Scanner.nextDouble();
-                }
-            }
-            
+            llenarTiempos();
+
             //average
-            
-            double sum=0;
-            for(int i=0;i<b;i++){
-                for(int j=0;j<5;j++){
-                    sum+=tiempos[i][j];
-                }
-                suma[i]=sum;
-                promedio[i]=sum/5;
-                sum=0;
-                
-            }
+            calculoPromedio();
             
             // resting the time with average and square all
-            
-            double pot;
-            for(int i=0;i<b;i++){
-                suma[i]=0;
-                for(int j=0;j<5;j++){
-                    pot=Math.pow(tiempos[i][j]-promedio[i],2);
-                    suma[i]+=pot;
-                    
-                }
-                desviacion[i]=Math.sqrt(suma[i]/4);
-            }
+            calculoDesviacion();
             
             //speed and accelerate
-            
-            for(int i=0;i<b;i++) {
-                velocidad[i]=distancia[i]/promedio[i];
-                aceleracion[i]=velocidad[i]/promedio[i];
-            }
+            calculoVelocidad();
             
             System.out.println("\n\n\nTabla Completa\n\n");
             
@@ -128,86 +83,25 @@ public class tablas{
                 System.out.print("        "+String.format("%.2f", velocidad[i])
                     +"       "+String.format("%.2f", aceleracion[i])+"\n\n");
             }
+
+            resultDesviacion();
             
-            int cont=0;
-            System.out.println("\n");
-            System.out.println("intervalo  desviacion  resultado");
-            for(int i=0;i<b;i++) {
-                System.out.print(distancia[i]+"        "+String.format("%.2f",desviacion[i]*100)+"       ");
-                if(desviacion[i]*100>6) {
-                    System.out.println("\033[31m AlTA \033[0m");
-                    cont++;
-                }
-                else
-                    System.out.println("\033[32m Buena \033[0m");
-            }
-            if(cont>0)
-            System.out.println("\nHay "+cont+" Desviaciones altas. Se debe Repetir esos tiempos");
-        }
+           
+    }
         
         //Metodo de la tabla del MRU
     private static void MRU() {
-            
-            //con los intervalos me refiero a en cuanto dividieron la distancia
-            
-            @SuppressWarnings("resource")
-            Scanner Scanner= new Scanner(System.in);
-            int b;
-            System.out.print("cuantos intervalos de tiempo?: ");
-            b=Scanner.nextInt();
-            
-            double[] distancia=new double[5];
-            double[] suma=new double[b], promedio=new double[b];
-            double[] velocidad=new double[b]; 
-            double []desviacion=new double[b];
-            double[][] tiempos=new double[b][5];		
-            
-            
-            //llenar
-            //se llena la distancia y los tiempos
-            System.out.println("digite la distancia y luego los tiempos de seguido por intervalo");
-            for(int i=0;i<b;i++){
-                System.out.println("intervalo: "+(i+1));
-                System.out.print("\ndistancia: ");
-                distancia[i]=Scanner.nextDouble();
-                
-                System.out.print("\ntiempos: ");
-                for(int j=0;j<5;j++){
-                        tiempos[i][j]=Scanner.nextDouble();
-                }
-            }
-            
-        
+
+         llenarTiempos();
             //promedio
-            
-            double sum=0;
-            for(int i=0;i<b;i++){
-                for(int j=0;j<5;j++){
-                    sum+=tiempos[i][j];
-                }
-                suma[i]=sum;
-                promedio[i]=sum/5;
-                sum=0;
-                
-            }
-            
-            //se resta a los tiempos el promedio y se eleva al cuadrado
-            double pot;
-            for(int i=0;i<b;i++){
-                suma[i]=0;
-                for(int j=0;j<5;j++){
-                    pot=Math.pow(tiempos[i][j]-promedio[i],2);
-                    suma[i]+=pot;
-                    
-                }
-                desviacion[i]=Math.sqrt(suma[i]/4);
-            }
+            calculoPromedio();
+
+            //desviacion
+            calculoDesviacion();
             
             //speed
+            calculoVelocidad();
             
-            for(int i=0;i<b;i++) {
-                velocidad[i]=distancia[i]/promedio[i];
-            }
             
             System.out.println("\n\n\nTabla Completa\n\n");
             
@@ -233,90 +127,31 @@ public class tablas{
                             
                 System.out.print("        "+String.format("%.2f", velocidad[i])+"\n\n");
             }
-            
-            int cont=0;
-            System.out.println("\n");
-            System.out.println("intervalo  desviacion  resultado");
-            for(int i=0;i<b;i++) {
-                System.out.print(distancia[i]+"        "+String.format("%.2f",desviacion[i]*100)+"       ");
-                if(desviacion[i]*100>6) {
-                    System.out.println("\033[31m AlTA \033[0m");
-                    cont++;
-                }
-                else
-                    System.out.println("\033[32m Buena \033[0m");
-            }
-            if(cont>0)
-            System.out.println("\nHay "+cont+" Desviaciones altas. Se debe Repetir esos tiempos");
+
+            resultDesviacion();
+        
     }
         
         //tabla de la caida libre
     private static void caidaLibre() {
             
-            //con los intervalos me refiero a en cuanto dividieron la distancia
-            
-            @SuppressWarnings("resource")
-            Scanner Scanner= new Scanner(System.in);
-            int b;
-            System.out.print("cuantos intervalos de tiempo?: ");
-            b=Scanner.nextInt();
-            
-            double[] distancia=new double[5];
-            double[] suma=new double[b], promedio=new double[b];
-            double[] velocidad=new double[b]; 
-            double []desviacion=new double[b];
-            double[][] tiempos=new double[b][5];		
-            double[] acceGravedad = new double[b];
-            double[] porcen = new double[b];
-            
-            //llenar
-            //se llena la distancia y los tiempos
-            System.out.println("digite la distancia y luego los tiempos de seguido por intervalo");
-            for(int i=0;i<b;i++){
-                System.out.println("intervalo: "+(i+1));
-                System.out.print("\ndistancia: ");
-                distancia[i]=Scanner.nextDouble();
-                
-                System.out.print("\ntiempos: ");
-                for(int j=0;j<5;j++){
-                        tiempos[i][j]=Scanner.nextDouble();
-                }
-            }
+        llenarTiempos();
+
+        acceGravedad = new double[b];
+        porcen = new double[b];
+       
             
         
             //promedio
-            
-            double sum=0;
-            for(int i=0;i<b;i++){
-                for(int j=0;j<5;j++){
-                    sum+=tiempos[i][j];
-                }
-                suma[i]=sum;
-                promedio[i]=sum/5;
-                sum=0;
-                
-            }
-            
-            //se resta a los tiempos el promedio y se eleva al cuadrado
-            double pot;
-            for(int i=0;i<b;i++){
-                suma[i]=0;
-                for(int j=0;j<5;j++){
-                    pot=Math.pow(tiempos[i][j]-promedio[i],2);
-                    suma[i]+=pot;
-                    
-                }
-                desviacion[i]=Math.sqrt(suma[i]/4);
-            }
-            
+            calculoPromedio();
+           
+            //desviacion
+            calculoDesviacion();
+
             //speed and gravity accelerate  
+            calculoVelocidad();
             
-            for(int i=0;i<b;i++) {
-                velocidad[i]=distancia[i]/promedio[i];
-                
-                acceGravedad[i]= (2*distancia[i]/Math.pow(promedio[i],2))/100;
-            }
-            
+            //porcentaje de cuan cerca esta de la gravedad
             for(int i=0;i<b;i++) {
                 porcen[i]= acceGravedad[i]*100/9.78;
                 
@@ -326,11 +161,13 @@ public class tablas{
                     porcen[i]-=porcen[i];
                 }
             }
+
+            //se muestra la tabla
             
             System.out.println("\n\n\nTabla Completa\n\n");
             
             System.out.println("intervalos  tiempo1  tiempo2  tiempo3  tiempo4 tiempo5  "
-                              + "promedio   desviacion   velocidad   gravedad   acce.gravedad  des.gravedad");
+                              + "promedio   desviacion   velocidad   gravedad   acce.gravedad");
             
             for(int i=0;i<b;i++){
                 System.out.print(distancia[i]+"\t     ");                                            
@@ -361,26 +198,96 @@ public class tablas{
                 else
                     System.out.print("\033[31m"+String.format("%.2f", acceGravedad[i])+ "\033[0m");
                 
-                System.out.print("        "+"\n\n");
+                System.out.print("\n\n");
             }
+
+            resultDesviacion();
             
-            
-            int cont=0;
-            System.out.println("\n");
-            System.out.println("intervalo  desviacion  resultado");
-            for(int i=0;i<b;i++) {
-                System.out.print(distancia[i]+"        "+String.format("%.2f",desviacion[i]*100)+"       ");
-                if(desviacion[i]*100>6) {
-                    System.out.println("\033[31m AlTA \033[0m");
-                    cont++;
-                }
-                else
-                    System.out.println("\033[32m Buena \033[0m");
-            }
-            if(cont>0)
-            System.out.println("\nHay "+cont+" Desviaciones altas. Se debe Repetir esos tiempos");
-    
     }
-        
+
+    static void llenarTiempos(){
+            //con los intervalos me refiero a en cuanto dividieron la distancia
+            
+            @SuppressWarnings("resource")
+            Scanner Scanner= new Scanner(System.in);
+
+            System.out.print("cuantos intervalos de tiempo?: ");
+            b=Scanner.nextInt();
+
+            distancia=new double[5];
+            suma=new double[b]; 
+            promedio=new double[b];
+            velocidad=new double[b]; 
+            aceleracion=new double[b];
+            desviacion=new double[b];
+            tiempos=new double[b][5];
+
+            //fill out the distance and the time
+            
+            System.out.println("digite la distancia y luego los tiempos de seguido por intervalo");
+            for(int i=0;i<b;i++){
+                System.out.println("intervalo: "+(i+1));
+                System.out.print("\ndistancia: ");
+                distancia[i]=Scanner.nextDouble();
+                
+                System.out.print("\ntiempos: ");
+                for(int j=0;j<5;j++){
+                        tiempos[i][j]=Scanner.nextDouble();
+                }
+            }
+            
+    }
+
+
+    static void resultDesviacion(){
+    int cont=0;
+    System.out.println("\n");
+    System.out.println("intervalo  desviacion  resultado");
+    for(int i=0;i<b;i++) {
+        System.out.print(distancia[i]+"        "+String.format("%.2f",desviacion[i]*100)+"       ");
+        if(desviacion[i]*100>6) {
+            System.out.println("\033[31m AlTA \033[0m");
+            cont++;
+        }
+        else
+            System.out.println("\033[32m Buena \033[0m");
+    }
+    if(cont>0)
+    System.out.println("\nHay "+cont+" Desviaciones altas. Se debe Repetir esos tiempos");
+    }
+    static void calculoDesviacion(){
+         //se resta a los tiempos el promedio y se eleva al cuadrado
+         double pot;
+         for(int i=0;i<b;i++){
+             suma[i]=0;
+             for(int j=0;j<5;j++){
+                 pot=Math.pow(tiempos[i][j]-promedio[i],2);
+                 suma[i]+=pot;
+                 
+             }
+             desviacion[i]=Math.sqrt(suma[i]/4);
+         }
+
+    }
+    static void calculoPromedio(){
+        double sum=0;
+        for(int i=0;i<b;i++){
+            for(int j=0;j<5;j++){
+                sum+=tiempos[i][j];
+            }
+            suma[i]=sum;
+            promedio[i]=sum/5;
+            sum=0;
+            
+        }
+    }
+    static void calculoVelocidad(){
+        for(int i=0;i<b;i++) {
+            velocidad[i]=distancia[i]/promedio[i];
+            aceleracion[i]=velocidad[i]/promedio[i];
+            acceGravedad[i]= (2*distancia[i]/Math.pow(promedio[i],2))/100;
+        }
+    }
+    
 }
 
